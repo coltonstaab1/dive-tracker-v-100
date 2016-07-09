4ff08cd6d18f28f56ae43d6c477c3b878af786d2
coltonstaab1 <colton.staab@gmail.com>
2016-07-09 19:17:22 -0400

    Added event listeners to NavCtrl

--- app/assets/javascripts/nav/NavCtrl.js
+++ app/assets/javascripts/nav/NavCtrl.js
@@ -1,6 +1,22 @@
 function NavCtrl($scope, Auth) {
   $scope.signedIn = Auth.isAuthenticated;
-  $scope.logout = Auth.logout;
+  $scope.logout = Auth.logout
+
+  $scope.$on('devise:new-registration', function (e, user){
+    $scope.user = user;
+  });
+
+  $scope.$on('devise:login', function (e, user){
+    $scope.user = user;
+  });
+
+  $scope.$on('devise:logout', function (e, user){
+    $scope.user = {};
+  });
+  
+  Auth.currentUser().then(function(user){
+    $scope.user = user;
+  })
 }
 
 angular
