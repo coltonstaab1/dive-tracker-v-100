106af9a593cd1abb0746b0b4d10c3220723c0908
coltonstaab1 <colton.staab@gmail.com>
2016-07-12 11:03:45 -0400

    Added login and register states to the app.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -11,6 +11,16 @@ angular
         url: '/meets',
         templateUrl: 'meets/_meets.html',
         controller: 'MeetsController as meets'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'auth/_login.html',
+        controller: 'AuthCtrl'
+      })
+      .state('register', {
+        url: '/register',
+        templateUrl: 'auth/_register.html',
+        controller: 'AuthCtrl'
       });
 
       $urlRouterProvider.otherwise('/meets')
