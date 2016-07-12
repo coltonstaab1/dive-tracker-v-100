c4439b776e899c525d2c09fa46ac21aebab06b7a
coltonstaab1 <colton.staab@gmail.com>
2016-07-12 17:48:16 -0400

    Added meets to routes file and fixed typoo in the meets controller

--- app/controllers/meets_controller.rb
+++ app/controllers/meets_controller.rb
@@ -1,10 +1,11 @@
 class MeetsController < ApplicationController
   def index
+
     @meets = Meet.all
 
     respond_to do |f|
       f.html { render :index }
-      f.json { render json: @companies }
+      f.json { render json: @meets }
     end
   end
 
