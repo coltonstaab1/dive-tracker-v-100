2f8e7315dfe3300e484068a9e3fc171c9c6891a4
coltonstaab1 <colton.staab@gmail.com>
2016-07-12 21:32:55 -0400

    Created getMeet and getMeets functions for the MeetService

--- app/assets/javascripts/meets/MeetService.js
+++ app/assets/javascripts/meets/MeetService.js
@@ -1,4 +1,12 @@
-function MeetService() {
+ function MeetService($http) {
+
+  this.getMeets = function() {
+    return $http.get('http://localhost:3000/meets.json');
+  }
+
+  this.getMeet = function(id) {
+    return $http.get('http://localhost:3000/meets/' + id + '.json');
+  }
 
 }
 
