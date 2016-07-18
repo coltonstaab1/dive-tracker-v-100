82403a051992627ce51dc0972c76c03c4939bfaf
coltonstaab1 <colton.staab@gmail.com>
2016-07-18 17:43:10 -0400

    Added first and last name fields to the user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713194933) do
+ActiveRecord::Schema.define(version: 20160718214148) do
 
   create_table "dive_types", force: :cascade do |t|
     t.integer  "dive_number"
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 20160713194933) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "team_id"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
