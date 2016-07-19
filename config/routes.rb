2c1698b4d3f2c3b7780f9756b1e3cf1aaa98b402
coltonstaab1 <colton.staab@gmail.com>
2016-07-19 14:55:40 -0400

    Created team controller and added routes to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'angular#angular'
   resources :meets
+  resources :teams, only [:index, :show]
   resources :users, only: [:index, :show]
 end
