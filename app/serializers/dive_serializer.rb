27222166ac3ddf4984bcb957000600d19dae4b73
coltonstaab1 <colton.staab@gmail.com>
2016-07-19 12:48:20 -0400

    Updated dive and meets serializers

--- app/serializers/dive_serializer.rb
+++ app/serializers/dive_serializer.rb
@@ -1,3 +1,5 @@
 class DiveSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :dive_type_id, :score_1, :score_2, :score_3, :meet_id, :user_id
+  has_one :user
+  has_one :meet
 end
--- app/serializers/meet_serializer.rb
+++ app/serializers/meet_serializer.rb
@@ -1,3 +1,4 @@
 class MeetSerializer < ActiveModel::Serializer
   attributes :id, :description, :datetime
+  has_many :dives
 end
