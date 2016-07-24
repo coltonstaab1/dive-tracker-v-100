a8a55132dd3c0d5d0c774c78c8e1e9188028d048
coltonstaab1 <colton.staab@gmail.com>
2016-07-24 14:34:34 -0400

    Fixed the meet serialization so the dive_type data is served with each dive

--- app/serializers/dive_serializer.rb
+++ app/serializers/dive_serializer.rb
@@ -1,5 +1,5 @@
 class DiveSerializer < ActiveModel::Serializer
-  attributes :id, :dive_type_id, :score_1, :score_2, :score_3, :meet_id, :user_id
+  attributes :id, :score_1, :score_2, :score_3, :meet_id, :user_id, :dive_type
   has_one :dive_type
   has_one :user
   has_one :meet
