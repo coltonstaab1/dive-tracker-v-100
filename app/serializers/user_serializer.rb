e8ded8b0143c9286b26bfade48cfce0eb3eedce0
coltonstaab1 <colton.staab@gmail.com>
2016-07-25 13:36:39 -0400

    Updated meets show view and the users serializer to make the team available in the JSON

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email
+  attributes :id, :first_name, :last_name, :email, :team
   has_one :team
   has_many :meets
 end
