a8e2ffed96e1b957623e68570b08cec9737a2ba6
coltonstaab1 <colton.staab@gmail.com>
2016-08-10 18:01:19 -0400

    Stubbed out most of the new meets form and updated the new meets controller

--- app/assets/javascripts/meets/MeetsNewController.js
+++ app/assets/javascripts/meets/MeetsNewController.js
@@ -1,5 +1,15 @@
-function MeetsNewController() {
+function MeetsNewController($stateParams, Auth, $scope) {
 
+  Auth.currentUser().then(function(user){
+    $scope.current_user = user;
+  })
+
+  this.number = 6;
+  this.getNumber = function(num) {
+      return new Array(num);   
+  }
+
+  this.meet_id = $stateParams.id;
 }
 
 angular
