63eaac0e676edd239f6153dda565fb19c0b4f6aa
coltonstaab1 <colton.staab@gmail.com>
2016-08-14 11:12:38 -0500

    Changed user serialization to correctly show meets

--- app/serializers/dive_serializer.rb
+++ app/serializers/dive_serializer.rb
@@ -1,5 +1,5 @@
 class DiveSerializer < ActiveModel::Serializer
-  attributes :id, :score_1, :score_2, :score_3, :meet_id, :user_id, :dive_type
+  attributes :id, :score_1, :score_2, :score_3, :dive_type, :meet
   has_one :dive_type
   has_one :user
   has_one :meet
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :team
+  attributes :id, :first_name, :last_name, :email, :team, :meets
   has_one :team
   has_many :meets
   has_many :dives
